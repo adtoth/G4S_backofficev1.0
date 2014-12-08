95cfd30e1a69aa46cd9e5cc7ee9da2e27bc7faa0
adtoth <adtoth13@gmail.com>
2014-12-08 12:16:14 +0100

    G4S dummy

---
+++ G4S workspace/Futar_dummy_data/WebContent/util/Grouper.js
@@ -0,0 +1,35 @@
+jQuery.sap.declare("sap.ui.demo.myFiori.util.Grouper");
+
+sap.ui.demo.myFiori.util.Grouper = {
+
+	bundle : null, // somebody has to set this
+
+	PicType : function (oContext) {
+		var status = oContext.getProperty("PicType");
+		var text = sap.ui.demo.myFiori.util.Grouper.bundle.getText("StatusText" + status, "?");
+		return {
+			key: status,
+			text: text
+		};
+	},
+	
+	DelStatus : function (oContext) {
+		var status = oContext.getProperty("DelStatus");
+		var text = sap.ui.demo.myFiori.util.Grouper.bundle.getText("StatusText" + status, "?");
+		return {
+			key: status,
+			text: text
+		};
+	},
+	
+	SzallitasStatus : function (oContext) {
+		var status = oContext.getProperty("SzallitasStatus");
+		var text = sap.ui.demo.myFiori.util.Grouper.bundle.getText("StatusText" + status, "?");
+		return {
+			key: status,
+			text: text
+		};
+	}
+	
+
+};
---
+++ G4S workspace/Futar_dummy_data/WebContent/util/Setter.js
@@ -0,0 +1,16 @@
+jQuery.sap.declare("sap.ui.demo.myFiori.util.Setter");
+
+sap.ui.demo.myFiori.util.Setter = {
+	
+		changeText: function(evt){
+/*			var a = evt
+	    	if(sap.ui.getCore().getModel().getProperty(a.sPath + "/DelStatus") == "999"){
+	    		alert("folytat");
+	    		//szallitasiDetail.byId("setActive").setText("Folytat");
+			}
+			else{
+				alert("aktivál");
+				//szallitasiDetail.byId("setActive").setText("Aktivál");
+			}*/
+		}
+};
